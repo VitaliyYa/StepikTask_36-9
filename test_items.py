4bcddcfa060adeaebe03d302f633427753fe3805
Vitaliy Ya <vytaliy@live.ru>
2019-06-18 23:32:45 +0300

    Test OK

--- test_items.py
+++ test_items.py
@@ -1,15 +1,14 @@
 import pytest
 from selenium import webdriver
-# import time
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_item_add_to_basket(browser):
     browser.get(link)
-    # time.sleep(30)
+    time.sleep(5)
     basket = browser.find_element_by_css_selector(
         "form button.btn-add-to-basket")
     assert basket.text, "Element not found"
     print(basket.text)
-    
