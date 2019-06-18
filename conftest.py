45e2cdaebf9976ac7d54ea37bb4d56632c50b399
Vitaliy Ya <vytaliy@live.ru>
2019-06-18 23:21:17 +0300

    Review_1: https://github.com/Zulbukharov/selenium_test_items

--- conftest.py
+++ conftest.py
@@ -1,38 +1,27 @@
-'''
-Добавьте в файл conftest.py обработчик, который считывает из командной строки параметр language.
-Реализуйте в файле conftest.py логику запуска браузера с указанным языком пользователя.
-Браузер должен объявляться в фикстуре browser и передаваться в тест как параметр.
-'''
-
-import pytest
 from selenium import webdriver
+import pytest
 from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-  parser.addoption('--language', action='store', default='ru', help='Choose language: ru or es or fr')
+    parser.addoption('--language', action='store', default="ru",
+                     help="Choose language: ar ca cs da de el es fi fr it ko nl pl pt pt-br ro ru sk uk zh-hans en-gb")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-  language = request.config.getoption('language')
-  if language == "ru":
-    print("\nStart chrome languages ru for test..")
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': 'ru'})
-    browser = webdriver.Chrome(options=options)
-  elif language == "es":
-    print("\nStart chrome languages en for test..")
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': 'es'})
-    browser = webdriver.Chrome(options=options)
-  elif language == "fr":
-    print("\nStart chrome languages en for test..")
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': 'fr'})
-    browser = webdriver.Chrome(options=options)
-  else:
-    print(f'Язык {language} не поддерживается. Выберите ru, es или fr.')
-  yield browser
-  print("\nQuit browser..")
-  browser.quit()
+    languages = "ar ca cs da de el es fi fr it ko nl pl pt pt-br ro ru sk uk zh-hans en-gb "
+    language = request.config.getoption("language")
+    if (language + " ") in languages:
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option(
+            'prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    else:
+        print("\nlanguage {} not supported :(\ntry: ar ca cs da de el es fi fr it ko nl pl pt pt-br ro ru sk uk zh-hans en-gb".format(language))
+        pytest.fail("Wrong Language")
+        # assert 0
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,16 +1,15 @@
-'''
-В файл test_items.py напишите тест, который проверяет, что страница товара на сайте содержит кнопку добавления в корзину.
-Например, можно проверять товар, доступный по http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/.
-'''
+import pytest
+from selenium import webdriver
+# import time
 
-import time
-
-
-link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_button_add_to_cart(browser): 
-  browser.get(link)
-  # time.sleep(30)
-  button = browser.find_element_by_css_selector('button.btn-add-to-basket').get_attribute('value')
-  assert button == 'Добавить в корзину' or 'Añadir al carrito' or 'Ajouter au panier', 'Add to cart button not found. (Кнопка добавления в корзину не найдена)'
+def test_item_add_to_basket(browser):
+    browser.get(link)
+    # time.sleep(30)
+    basket = browser.find_element_by_css_selector(
+        "form button.btn-add-to-basket")
+    assert basket.text, "Element not found"
+    print(basket.text)
+    
