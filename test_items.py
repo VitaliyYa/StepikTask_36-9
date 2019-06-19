0582348e1654293c2c0f74988fa4793d98adf5a3
Vitaliy Ya <vytaliy@live.ru>
2019-06-19 08:03:48 +0300

    Test OK

--- test_items.py
+++ test_items.py
@@ -1,3 +1,6 @@
+import time
+
 def test_find_button_add_to_basket(browser):
+    time.sleep(5)
     btn = browser.find_elements_by_class_name("btn-add-to-basket")
     assert len(btn) > 0, "Element not found"
