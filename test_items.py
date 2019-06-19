f7ef41bbb98b89e1d32837ed999b4ec884a28019
Vitaliy Ya <vytaliy@live.ru>
2019-06-19 08:10:10 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -13,4 +13,4 @@ def test_button_add_to_cart(browser):
   browser.get(link)
   # time.sleep(30)
   button = browser.find_element_by_css_selector('button.btn-add-to-basket').get_attribute('value')
-  assert button == 'Добавить в корзину' or 'Añadir al carrito' or 'Ajouter au panier', 'Add to cart button not found. (Кнопка добавления в корзину не найдена)'
+  assert len(button) > 0, 'Add to cart button not found. (Кнопка добавления в корзину не найдена)'
