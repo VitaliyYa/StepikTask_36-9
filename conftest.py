c4a79923f78d5978169433a49f9714b44534abca
Vitaliy Ya <vytaliy@live.ru>
2019-06-26 15:26:12 +0300

    Added selection browser

--- conftest.py
+++ conftest.py
@@ -10,20 +10,34 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-  parser.addoption('--language', action='store', default='ru',
-  help='Choose language: ar ca cs da de el en es fi fr it ko nl pl pt pt-br ro ru sk uk zh-hans en-gb')
+    parser.addoption('--browser_name', action='store', default='chrome', help='"chrome" or "firefox".')
+    parser.addoption('--language', action='store', default='ru', help='Choose language: ar ca cs da de el en es fi fr it ko nl pl pt pt-br ro ru sk uk zh-hans en-gb')
+    # parser.addoption('--timeout', action='store', default=10, help='Choose timeout time (seconds)')
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-  languages = ['ar', 'ca', 'cs', 'da', 'de', 'el', 'en', 'es', 'fi', 'fr', 'it', 'ko', 'nl', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sk', 'uk', 'zh-hans', 'en-gb']
-  language = request.config.getoption('language')
-  if language in languages:
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': language})
-    browser = webdriver.Chrome(options=options)
-  else:
-    print(f'\nlanguage "{language}"" is not supported. Язык "{language}" не поддерживается.')
-    print(f'Try: {", ".join(languages)}.')
-  yield browser
-  browser.quit()
+    browser_name = request.config.getoption('browser_name')
+    # languages = ['ar', 'ca', 'cs', 'da', 'de', 'el', 'en', 'es', 'fi', 'fr', 'it', 'ko', 'nl', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sk', 'uk', 'zh-hans', 'en-gb']
+    # language = request.config.getoption('language')
+    language = request.config.getoption('language')
+
+    if browser_name == 'chrome':
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+        browser.implicitly_wait(5)
+    
+    elif browser_name == 'firefox':
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+        browser.implicitly_wait(5)
+
+    else:
+        print(f'\nBrowser "{browser_name}"" is not implemented. "chrome" and "firefox" only')
+
+    yield browser
+    browser.quit()
